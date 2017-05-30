0824f9098ed18260d8562f14b5adaf36b400a931
Alexander Miller <mial1014@hs-karlsruhe.de>
2017-05-30 17:38:51 +0200

    Kommentare entfernt

--- src/kunde/shared/kunde.ts
+++ src/kunde/shared/kunde.ts
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*Testkommentar von Yvonne*/
 
 import * as _ from 'lodash'
 import * as moment from 'moment'
