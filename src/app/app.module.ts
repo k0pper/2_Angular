85f2085a195ee7281073d2898618785afcb6e840
Alexander Miller <mial1014@hs-karlsruhe.de>
2017-05-31 09:43:46 +0200

    Imports alphabetisch sortiert (TSLint)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,8 +19,8 @@ import {NgModule} from '@angular/core'
 import {BrowserModule} from '@angular/platform-browser'
 
 import {AUTH_PROVIDERS} from '../auth/auth.module'
-import KundeModule from '../kunde/kunde.module'
 import HomeModule from '../home/home.module'
+import KundeModule from '../kunde/kunde.module'
 import LayoutModule from '../layout/layout.module'
 import SharedModule from '../shared/shared.module'
 
